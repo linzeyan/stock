16fc40ffa326f21afeaad280fe35d533cae2ca8a
Ricky <ricky@Macbook.local>
2018-12-25 20:50:27 +0800

    Add to_sql command

--- stock.py
+++ stock.py
@@ -8,31 +8,10 @@ import json
 import requests
 import pandas
 from bs4 import BeautifulSoup
+import MySQLdb
+from sqlalchemy import create_engine
+import pymysql
 
-"""Get stock info
-stock = '1234'
-path = 'data'
-
-folder_path = '{}/{}'.format(path, date.today().strftime('%Y%m%d'))
-if not os.path.isdir(folder_path):
-    os.makedirs(folder_path)
-
-endpoint = 'http://mis.twse.com.tw/stock/api/getStockInfo.jsp'
-timestamp = int(time.time() * 1000 + 1000000)
-channels = ''.join('tse_{}.tw'.format(stock))
-query_url = '{}?_={}&ex_ch={}'.format(endpoint, timestamp, channels)
-
-req = requests.session()
-req.get('http://mis.twse.com.tw/stock/index.jsp',headers={'Accept-Language': 'zh-TW'})
-
-response = req.get(query_url)
-content = json.loads(response.text)['msgArray']
-raw = pandas.DataFrame(content)
-raw.to_csv('{}/{}/{}_raw.csv'.format(path, date.today().strftime('%Y%m%d'), stock))
-
-result = pandas.DataFrame(content)[['c','n','o','h','l','tv']].rename(index=str, columns={'c': '代號','n': '名稱','o':'開盤','h':'最高','l':'最低','tv':'當日成交量'})
-result.to_csv('{}/{}/{}.csv'.format(path, date.today().strftime('%Y%m%d'), stock))
-"""
 #for row in content:
 #    try:
 #        file_path = '{}/{}.csv'.format(folder_path, row['c'])
@@ -54,6 +33,12 @@ result.to_csv('{}/{}/{}.csv'.format(path, date.today().strftime('%Y%m%d'), stock
 
 path = 'files/stock'
 folder_path = '{}/{}'.format(path, date.today().strftime('%Y%m%d'))
+dbhost = '127.0.0.1'
+dbport = 3306
+dbname = 'stock_in'
+dbuser = 'root'
+dbpass = '1234qwer'
+engine = create_engine(str(r"mysql+pymysql://%s:" + '%s' + "@%s:%s/%s?charset=utf8") % (dbuser, dbpass, dbhost,dbport, dbname))
 
 if not os.path.isdir(folder_path):
       os.makedirs(folder_path)
@@ -98,9 +83,11 @@ for number in stock_id:
         content = json.loads(response.text)['msgArray']
         raw = pandas.DataFrame(content)
         raw.to_csv('{}/{}/{}_raw.csv'.format(path, date.today().strftime('%Y%m%d'), stock))
+        raw.to_sql(name='tb_raw',con=engine,if_exists='append',index=False)
     
         result = pandas.DataFrame(content)[['d','c','n','o','h','l','tv']].rename(index=str, columns={'d': '日期', 'c': '代號','n': '名稱','o':'開盤','h':'最高','l':'最低','tv':'當日成交量'})
         result.to_csv('{}/{}/{}.csv'.format(path, date.today().strftime('%Y%m%d'), stock))
+        result.to_sql(name='tb_filter',con=engine,if_exists='append',index=False)
     except KeyError:
         print('Error ! stock : '+stock+' need to query again.')
         continue
